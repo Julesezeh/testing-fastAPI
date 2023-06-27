6f81b4ea8dbc524114f1b59e1ede33d7a91965e5
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-06-27 14:49:35 +0100

    two more view functions

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ async def root():
 
 @app.get("/number")
 async def rando():
+    randint: int = random.randint(0, 100)
     # symbols = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10}
-    return random.randint(0, 100)
+    return randint
+
+
+@app.get("/number/{num}")
+async def spec_range_rando(num: int):
+    randint = random.randint(0, num)
+    return {"number generated": randint, "limit set by you": num}
