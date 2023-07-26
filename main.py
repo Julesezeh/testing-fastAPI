d9d9d33161af0e90d75857d8c6cbcc4f3c8b7f48
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-26 09:57:53 +0100

    main server

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ async def root():
     return {"name": "Jules", "mentality": "Slime"}
 
 
-@app.get("/number")
+@app.get("/number/")
 async def rando():
     randint: int = random.randint(0, 100)
     # symbols = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10}
     return randint
 
 
-@app.get("/number/{num}")
+@app.get("/number/{num}/")
 async def spec_range_rando(num: int):
     randint = random.randint(0, num)
-    return {"number generated": randint, "limit set by you": num}
+    list_of_rando = [x for x in range(0, randint)]
+    return {"number range generated": list_of_rando, "limit set by you": num}
