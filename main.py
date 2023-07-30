3624e7cda71007ec9b1e8c621002d1f928ddfade
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-30 08:27:36 +0100

    multiple query parameters

--- main.py
+++ main.py
@@ -64,3 +64,18 @@ async def users(username: ModelName):
 @app.get("/items/")
 async def read_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+# Multiple query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
