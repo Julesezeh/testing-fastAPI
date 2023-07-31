727e6bd0b79c452742f8340c0695217f7ce1ecf4
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-31 08:50:21 +0100

    gotten a hang of pydantic

--- models.py
+++ models.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel
+from enum import Enum
+from uuid import UUID, uuid4
+from typing import List
 
 
-# class User(BaseModel):
-#     id:
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+
+class Role(str, Enum):
+    admin = "admin"
+    student = "student"
+    user = "user"
+    janitor = "janitor"
+
+
+class Users(BaseModel):
+    id: UUID = uuid4()
+    first_name: str
+    last_name: str
+    gender: Gender
+    role: List[Role]
